049def9fdab82e2c47b66199c40cf886b6663146
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-02-18 12:06:06 +0100

    metode abstracte

--- truc/rols/player/player.py
+++ truc/rols/player/player.py
@@ -1,4 +1,7 @@
-class TrucPlayer:
+from abc import ABC, abstractmethod
+
+
+class TrucPlayer(ABC):
     def __init__(self, player_id, np_random):
         self.player_id = player_id
         self.np_random = np_random
@@ -7,3 +10,7 @@ class TrucPlayer:
 
     def get_player_id(self):
         return self.player_id
+
+    @abstractmethod
+    def triar_accio(self, estat):
+        pass
