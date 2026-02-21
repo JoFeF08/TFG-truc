2572c1a416281b4bafeabbd97d0c38b2d1acb1ef
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-02-21 19:21:33 +0100

    ficar espara a jugador maquina

--- entorn/rols/player/player_random.py
+++ entorn/rols/player/player_random.py
@@ -1,3 +1,5 @@
+import time
+
 from entorn.rols.player.player import TrucPlayer
 
 
@@ -7,13 +9,13 @@ class RandomPlayer(TrucPlayer):
     Serveix com a baseline per comparar agents més intel·ligents.
     """
 
-    def __init__(self, player_id, np_random):
+    def __init__(self, player_id, np_random, time_ms=600):
         super().__init__(player_id, np_random)
+        self._time_ms = time_ms
 
     def triar_accio(self, estat):
+        time.sleep(self._time_ms / 500)
         accions_legals = estat['accions_legals']
-        
         if len(accions_legals) == 1:
             return accions_legals[0]
-
         return self.np_random.choice(accions_legals)
