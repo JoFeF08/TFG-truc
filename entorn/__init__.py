23268820f3b615e85d990e34c283a1897da4db78
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-02-22 21:18:47 +0100

    recollido partides

--- entorn/__init__.py
+++ entorn/__init__.py
@@ -2,5 +2,13 @@ from entorn.cartes_accions import ACTION_LIST
 from entorn.env import TrucEnv
 from entorn.rols.player.player_huma import HumanPlayer
 from entorn.rols.player.player_random import RandomPlayer
+from entorn.data_collector import DataCollector, carregar_partides
 
-__all__ = ["TrucEnv", "ACTION_LIST", "HumanPlayer", "RandomPlayer"]
+__all__ = [
+    "TrucEnv",
+    "ACTION_LIST",
+    "HumanPlayer",
+    "RandomPlayer",
+    "DataCollector",
+    "carregar_partides",
+]
