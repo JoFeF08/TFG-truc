6c98982082b6cbc1343d70bab32541dd4e8a504e
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-02-27 14:03:16 +0100

    adaptor models rcard

---
+++ entorn/rols/player/models/__init__.py
@@ -0,0 +1,7 @@
+from entorn.rols.player.models.base import TrucModel
+from entorn.rols.player.models.rlcard_adapter import RLCardModelAdapter
+
+__all__ = [
+    "TrucModel",
+    "RLCardModelAdapter",
+]
