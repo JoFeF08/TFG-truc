12bc67d358b33e4479b08bab3e0a7c554151b8ca
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-03-04 22:28:11 +0100

    nova carpeta per els entrenamentde de RLCard

--- entrenament/entrenamentRLCard/entrenament_nfsp.py
+++ entrenament/entrenamentRLCard/entrenament_nfsp.py
@@ -2,7 +2,7 @@ import sys
 import os
 from pathlib import Path
 from datetime import datetime
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 import csv
 import torch
