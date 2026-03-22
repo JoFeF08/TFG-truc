e6cc32c1a835c9e7fb742393a5de614fb65b2df2
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2026-03-22 22:25:26 +0100

    eliminar data colector

--- joc/entorn/__init__.py
+++ joc/entorn/__init__.py
@@ -1,11 +1,8 @@
 from joc.entorn.cartes_accions import ACTION_LIST
 from joc.entorn.env import TrucEnv, reorganize_amb_rewards
-from joc.entorn.data_collector import DataCollector, carregar_partides
 
 __all__ = [
     "TrucEnv",
     "reorganize_amb_rewards",
     "ACTION_LIST",
-    "DataCollector",
-    "carregar_partides",
 ]
